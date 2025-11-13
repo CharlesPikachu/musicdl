944cff7403d5602c7696abd377a8d81e238ea7e1
CharlesPikachu <1159254961@qq.com>
2025-11-13 11:46:28 +0800

    fix searchlyrics

--- examples/searchlyrics/searchlyrics.py
+++ examples/searchlyrics/searchlyrics.py
@@ -1,51 +1,53 @@
 '''
 Function:
-    根据歌词获取对应的mp3文件片段
+    Implementation of SearchLyrics
 Author:
-    Charles
-微信公众号:
+    Zhenchao Jin
+WeChat Official Account (微信公众号):
     Charles的皮卡丘
 '''
 import re
 import os
-from musicdl import musicdl
 from pydub import AudioSegment
 from pydub.playback import play
+from musicdl.modules.sources import NeteaseMusicClient
+from musicdl.modules.utils.misc import sanitize_filepath
 
 
-'''根据歌词获取对应的mp3文件片段'''
+'''SearchLyrics'''
 class SearchLyrics():
     def __init__(self):
-        self.rootdir = os.path.split(os.path.abspath(__file__))[0]
-        self.target_srcs = ['netease']
-        self.config = {'logfilepath': 'musicdl.log', 'savedir': self.rootdir, 'search_size_per_source': 1, 'proxies': {}}
-        self.api_client = musicdl.musicdl(config=self.config)
-    '''调用运行'''
-    def run(self):
-        keyword = input('请输入歌词: ')
-        print('[INFO]: 正在尝试获取对应的歌曲')
-        results = self.api_client.search(keyword, self.target_srcs)[self.target_srcs[0]]
-        self.api_client.download(results)
-        print('[INFO]: 正在尝试提取对应歌词的音频')
-        lyrics = results[0]['lyric']
+        self.music_client = NeteaseMusicClient(search_size_per_source=1)
+    '''start'''
+    def start(self):
+        # inputs
+        keyword = input('Input lyrics: ')
+        # search and download music files
+        print('[INFO]: Searching and downloading music files by lyrics')
+        song_infos = self.music_client.search(keyword)[:1]
+        self.music_client.download(song_infos)
+        # extract audio clips
+        print('[INFO]: Extract the corresponding audio clip and play')
+        lyrics: str = song_infos[0]['lyric']
         start, end = None, None
         for lyric in lyrics.split('\n'):
             if start is not None and end is not None:
                 break
             if keyword in lyric:
-                start = re.findall(r'\[(.*?)\]', lyric)[0]
+                start: str = re.findall(r'\[(.*?)\]', lyric)[0]
                 start = int(float(start.split(':')[0])) * 1000 * 60 + int(float(start.split(':')[1])) * 1000 - 1000
             elif start is not None:
-                end = re.findall(r'\[(.*?)\]', lyric)[0]
+                end: str = re.findall(r'\[(.*?)\]', lyric)[0]
                 end = int(float(end.split(':')[0])) * 1000 * 60 + int(float(end.split(':')[1])) * 1000 + 1000
-        filepath = os.path.join(self.rootdir, f"{results[0]['savename']}.{results[0]['ext']}")
-        music = AudioSegment.from_mp3(file=filepath)
+        # play
+        file_path = os.path.join(song_infos[0]['work_dir'], f"{song_infos[0]['song_name']}.{song_infos[0]['ext']}")
+        music = AudioSegment.from_mp3(file=sanitize_filepath(file_path))
         music_cut = music[start: end]
-        music_cut.export(out_f=os.path.join(self.rootdir, f"{results[0]['savename']}_cut.{results[0]['ext']}"), format='mp3')
+        music_cut.export(out_f=os.path.join(song_infos[0]['work_dir'], f"{song_infos[0]['song_name']}_cut.{song_infos[0]['ext']}"), format='mp3')
         play(music_cut)
-        
 
-'''run'''
+
+'''tests'''
 if __name__ == '__main__':
     client = SearchLyrics()
-    client.run()
+    client.start()
