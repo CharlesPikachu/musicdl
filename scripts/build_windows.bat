35513c04777a5d28d253aef4e1d9d199639ed6d1
NZH <nzh20020511@gmail.com>
2026-02-01 14:31:39 +0800

    build: MusicDL_v2.8.12

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -4,13 +4,16 @@ python -m pip install PyQt6 pyinstaller -r ..\requirements.txt
 
 echo Building Executable...
 pyinstaller --noconfirm --noconsole --onefile --windowed --icon="../icon.ico" ^
-    --name "MusicDL_v2.8.9" ^
+    --name "MusicDL_v2.8.12" ^
     --add-data "../musicdl;musicdl" ^
     --add-data "../icon.ico;." ^
     --hidden-import "PyQt6" ^
     --hidden-import "requests" ^
     --hidden-import "rich" ^
     --hidden-import "click" ^
+    --hidden-import "musicdl.modules.audiobooks.lizhi" ^
+    --hidden-import "musicdl.modules.audiobooks.qingting" ^
+    --hidden-import "musicdl.modules.audiobooks.ximalaya" ^
     --hidden-import "musicdl.modules.sources.apple" ^
     --hidden-import "musicdl.modules.sources.bilibili" ^
     --hidden-import "musicdl.modules.sources.buguyy" ^
@@ -28,9 +31,7 @@ pyinstaller --noconfirm --noconsole --onefile --windowed --icon="../icon.ico" ^
     --hidden-import "musicdl.modules.sources.kugou" ^
     --hidden-import "musicdl.modules.sources.kuwo" ^
     --hidden-import "musicdl.modules.sources.livepoo" ^
-    --hidden-import "musicdl.modules.sources.lizhi" ^
     --hidden-import "musicdl.modules.sources.migu" ^
-    --hidden-import "musicdl.modules.sources.missevan" ^
     --hidden-import "musicdl.modules.sources.mitu" ^
     --hidden-import "musicdl.modules.sources.netease" ^
     --hidden-import "musicdl.modules.sources.qianqian" ^
@@ -38,10 +39,11 @@ pyinstaller --noconfirm --noconsole --onefile --windowed --icon="../icon.ico" ^
     --hidden-import "musicdl.modules.sources.soda" ^
     --hidden-import "musicdl.modules.sources.tidal" ^
     --hidden-import "musicdl.modules.sources.twot58" ^
-    --hidden-import "musicdl.modules.sources.ximalaya" ^
     --hidden-import "musicdl.modules.sources.yinyuedao" ^
     --hidden-import "musicdl.modules.sources.youtube" ^
+    --hidden-import "musicdl.modules.sources.zhuolin" ^
     --hidden-import "musicdl.modules.common.gdstudio" ^
+    --hidden-import "musicdl.modules.common.jbsou" ^
     --hidden-import "musicdl.modules.common.mp3juice" ^
     --hidden-import "musicdl.modules.common.myfreemp3" ^
     --hidden-import "musicdl.modules.common.tunehub" ^
