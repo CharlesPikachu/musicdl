dc96e7bbb2ac7012a3d979e9802a4da6b4219e08
Type-Cargeing <nzh20020511@gmail.com>
2026-02-14 16:38:47 +0800

    build: 2.9.12

--- musicdl/musicdl_gui.py
+++ musicdl/musicdl_gui.py
@@ -48,7 +48,7 @@ def main():
     # Set application metadata
     app.setApplicationName("MusicDL")
     app.setOrganizationName("MusicDL")
-    app.setApplicationVersion("2.9.11")
+    app.setApplicationVersion("2.9.12")
     
     # Create and show main window
     window = MainWindow()
