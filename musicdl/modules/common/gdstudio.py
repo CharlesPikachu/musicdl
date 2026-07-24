82e480d9c0ea8b2b2820d845965287b1e60cf4e8
CharlesPikachu <blwx96@connect.hku.hk>
2026-07-25 06:16:31 +0800

    all common music client re-passed now

--- musicdl/modules/common/gdstudio.py
+++ musicdl/modules/common/gdstudio.py
@@ -20,19 +20,19 @@ from ..utils import resp2json, legalizestring, usesearchheaderscookies, safeextr
 '''GDStudioMusicClient'''
 class GDStudioMusicClient(BaseMusicClient):
     source = 'GDStudioMusicClient'
-    VERSION = "2026.06.16"
+    VERSION = "2026.07.21"
     HOST = "music.gdstudio.xyz"
     BASE_URL = "https://music.gdstudio.xyz/"
     TIME_URL = "https://music.gdstudio.xyz/time"
     API_URL = "https://music.gdstudio.xyz/api.php"
     MUSIC_QUALITIES = [999, 740, 320, 192, 128]
-    SUPPORTED_SITES = ['netease', 'joox', 'spotify', 'tidal', 'qobuz', 'apple', 'ytmusic', 'bilibili', 'kuwo', 'tencent']
+    SUPPORTED_SITES = ['netease', 'joox', 'tidal', 'qobuz', 'apple', 'bilibili', 'ytmusic', 'spotify', 'kuwo', 'tencent']
     def __init__(self, **kwargs):
-        self.allowed_music_sources = list(set(kwargs.pop('allowed_music_sources', GDStudioMusicClient.SUPPORTED_SITES[:-2])))
+        self.allowed_music_sources = list(set(kwargs.pop('allowed_music_sources', GDStudioMusicClient.SUPPORTED_SITES[:-4])))
         super(GDStudioMusicClient, self).__init__(**kwargs)
         self.default_search_headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36", "Origin": "https://music.gdstudio.xyz",
-            "Referer": "https://music.gdstudio.xyz/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Referer": "https://music.gdstudio.xyz/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01", "Origin": "https://music.gdstudio.xyz", 
         }
         self.default_download_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36'}
         self.default_bilibili_download_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36', 'Referer': 'https://www.bilibili.com/'}
@@ -53,12 +53,12 @@ class GDStudioMusicClient(BaseMusicClient):
     def gdstudiomd5(message: str) -> str:
         bit_len = (len((data := message.encode("utf-8"))) * 8) & 0xFFFFFFFFFFFFFFFF; data += b"\x80"
         while len(data) % 64 != 56: data += b"\x00"
-        data += struct.pack("<Q", bit_len); a0 = 0x67452302; b0 = 0xEFCDAB8A; c0 = 0x98BADCFE; d0 = 0x10325476
+        data += struct.pack("<Q", bit_len); a0 = 0x67452301; b0 = 0xEFCDAB89; c0 = 0x98BADCFE; d0 = 0x10325476
         shifts = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
         table = [
-            0xD76AA479, 0xE8C7B756, 0x242070DB, 0xC1BDCEEE, 0xF57C0FAF, 0x4787C62A, 0xA8304613, 0xFD469501, 0x698098D8, 0x8B44F7AF, 0xFFFF5BB1, 0x895CD7BE, 0x6B901122, 0xFD987193, 0xA679438E, 0x49B40821,
+            0xD76AA478, 0xE8C7B756, 0x242070DB, 0xC1BDCEEE, 0xF57C0FAF, 0x4787C62A, 0xA8304613, 0xFD469501, 0x698098D8, 0x8B44F7AF, 0xFFFF5BB1, 0x895CD7BE, 0x6B901122, 0xFD987193, 0xA679438E, 0x49B40821,
             0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4C8A,
-            0xFFFA3942, 0x8771F681, 0x6D9D6122, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
+            0xFFFA3942, 0x8771F681, 0x6D9D6123, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
             0xF4292244, 0x432AFF97, 0xAB9423A7, 0xFC93A039, 0x655B59C3, 0x8F0CCC92, 0xFFEFF47D, 0x85845DD1, 0x6FA87E4F, 0xFE2CE6E0, 0xA3014314, 0x4E0811A1, 0xF7537E82, 0xBD3AF235, 0x2AD7D2BB, 0xEB86D391,
         ]
         for offset in range(0, len(data), 64):
@@ -134,7 +134,7 @@ class GDStudioMusicClient(BaseMusicClient):
         try:
             # --search results
             (resp := self.post(GDStudioMusicClient.API_URL, data=post_data, timeout=10, **request_overrides)).raise_for_status()
-            for search_result_idx, search_result in enumerate(resp2json(resp=resp)):
+            for search_result_idx, search_result in enumerate(resp2json(resp=resp)[:self.search_size_per_page]):
                 # --update progress
                 progress.update(task_id, description=f"{self.source}.{root_source}._search >>> Start to process the {search_result_idx+1}th search result on page {page_no}", completed=search_result_idx+1, total=search_result_idx+1)
                 # --download results
