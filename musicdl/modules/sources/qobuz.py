c5a44b1eb839b6ef15a82652c71f3a56148d1941
CharlesPikachu <blwx96@connect.hku.hk>
2026-09-10 00:29:14 +0800

    refactor qobuz music clients and update all expired third part apis

--- musicdl/modules/sources/qobuz.py
+++ musicdl/modules/sources/qobuz.py
@@ -16,10 +16,11 @@ import tempfile
 import requests
 import threading
 from contextlib import suppress
-from .base import BaseMusicClient
+from typing_extensions import Unpack
 from ..utils.zarz import ZarzQobuzClient
 from ..utils.hosts import QOBUZ_MUSIC_HOSTS
 from ..utils.afkarxyz import QobuzCommunityClient
+from .base import BaseMusicClient, BaseMusicClientKwargs
 from pathvalidate import sanitize_filepath, sanitize_filename
 from urllib.parse import urlencode, urlparse, parse_qs, quote
 from ..utils.qobuzutils import QobuzMusicClientUtils, ArcodClient
@@ -31,9 +32,9 @@ from ..utils import legalizestring, resp2json, usesearchheaderscookies, safeextr
 class QobuzMusicClient(BaseMusicClient):
     source = 'QobuzMusicClient'
     arcod_client = ArcodClient(base_url="https://arcod.xyz")
-    qobuz_community_client = QobuzCommunityClient(app_version='7.2.0')
+    qobuz_community_client = QobuzCommunityClient(app_version='7.2.2')
     qobuz_community_lock = threading.RLock()
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Unpack[BaseMusicClientKwargs]):
         super(QobuzMusicClient, self).__init__(**kwargs)
         if self.default_search_cookies: assert QobuzMusicClientUtils.get_token_func(self.default_search_cookies, "user_auth_token", "X-User-Auth-Token", "x-user-auth-token"), '"x-user-auth-token" should be configured, refer to "https://musicdl.readthedocs.io/en/latest/Clients.html#qobuzmusicclient-built-in-premium-account"'
         if self.default_parse_cookies: assert QobuzMusicClientUtils.get_token_func(self.default_parse_cookies, "user_auth_token", "X-User-Auth-Token", "x-user-auth-token"), '"x-user-auth-token" should be configured, refer to "https://musicdl.readthedocs.io/en/latest/Clients.html#qobuzmusicclient-built-in-premium-account"'
@@ -157,29 +158,11 @@ class QobuzMusicClient(BaseMusicClient):
         song_info = SongInfo(source=self.source, raw_data={'quality': QobuzMusicClientUtils.MUSIC_QUALITIES[-1]}) if (song_info.file_size_bytes * 8 < 320000 * song_info.duration_s) else song_info
         # return
         return song_info
-    '''_parsewithcyrusna29api'''
-    def _parsewithcyrusna29api(self, search_result: dict, request_overrides: dict = None):
-        # init
-        request_overrides, song_id, headers = request_overrides or {}, str(search_result['id']), {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36", "Referer": "https://qobuz-tidal-eclipse.cyrusna29.workers.dev/"}
-        # parse
-        (resp := requests.post(f"https://qobuz-tidal-eclipse.cyrusna29.workers.dev/generate", json={"quality": "HIRES"}, headers=headers, timeout=10, **request_overrides)).raise_for_status()
-        api_url = f"https://qobuz-tidal-eclipse.cyrusna29.workers.dev/u/{resp2json(resp=resp)['token']}/stream/{song_id}"
-        (resp := requests.get(api_url, headers=headers, timeout=10, **request_overrides)).raise_for_status()
-        download_url = safeextractfromdict((download_result := resp2json(resp=resp)), ['url'], '')
-        real_music_quality = real_music_quality[0] if isinstance((real_music_quality := parse_qs(urlparse(str(download_url)).query, keep_blank_values=True).get('fmt') or QobuzMusicClientUtils.MUSIC_QUALITIES[0]), list) else real_music_quality
-        download_url_status: dict = self.audio_link_tester.test(url=download_url, request_overrides=request_overrides, renew_session=True)
-        song_info = SongInfo(
-            raw_data={'search': search_result, 'download': download_result, 'lyric': {}, 'quality': real_music_quality}, source=self.source, song_name=legalizestring(search_result.get('title')), singers=legalizestring(safeextractfromdict(search_result, ['performer', 'name'], None)), album=legalizestring(safeextractfromdict(search_result, ['album', 'title'], None)), ext=download_url_status['ext'], file_size_bytes=download_url_status['file_size_bytes'], 
-            file_size=download_url_status['file_size'], identifier=song_id, duration_s=int(float(search_result.get('duration') or 0)), duration=SongInfoUtils.seconds2hms(int(float(search_result.get('duration') or 0))), lyric=None, cover_url=safeextractfromdict(search_result, ['album', 'image', 'large'], None), download_url=download_url_status['download_url'], download_url_status=download_url_status, 
-        )
-        song_info = SongInfo(source=self.source, raw_data={'quality': QobuzMusicClientUtils.MUSIC_QUALITIES[-1]}) if (song_info.file_size_bytes * 8 < 320000 * song_info.duration_s) else song_info
-        # return
-        return song_info
     '''_parsewithgdstudioxyzapi'''
     def _parsewithgdstudioxyzapi(self, search_result: dict, request_overrides: dict = None):
         # init
-        host = "music.gdstudio.xyz"; version = "2026.07.21"; time_url = "https://music.gdstudio.xyz/time"; api_url = "https://music.gdstudio.xyz/api.php"
-        request_overrides, song_id, headers = request_overrides or {}, str(search_result['id']), {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36", "Origin": "https://music.gdstudio.xyz", "Referer": "https://music.gdstudio.xyz/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+        host = "music.gdstudio.xyz"; version = "2026.08.01"; time_url = "https://music.gdstudio.xyz/time"; api_url = "https://music.gdstudio.xyz/api.php"
+        request_overrides, song_id, headers = request_overrides or {}, str(search_result['id']), {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36", "Origin": "https://music.gdstudio.xyz", "Referer": "https://music.gdstudio.xyz/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
         js_encode_uri_component_func = lambda value: (quote(str(value), safe="-_.!~*'()").replace("(", "%28").replace(")", "%29").replace("*", "%2A").replace("'", "%27").replace("!", "%21"))
         left_rotate_func = lambda x, amount: ((((x & 0xFFFFFFFF) << amount) | ((x & 0xFFFFFFFF) >> (32 - amount))) & 0xFFFFFFFF)
         normalize_version_func = lambda value: "".join(part.zfill(2) if len(part) == 1 else part for part in str(value).split("."))
@@ -192,8 +175,8 @@ class QobuzMusicClient(BaseMusicClient):
         shifts = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
         table = [
             0xD76AA478, 0xE8C7B756, 0x242070DB, 0xC1BDCEEE, 0xF57C0FAF, 0x4787C62A, 0xA8304613, 0xFD469501, 0x698098D8, 0x8B44F7AF, 0xFFFF5BB1, 0x895CD7BE, 0x6B901122, 0xFD987193, 0xA679438E, 0x49B40821,
-            0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4C8A,
-            0xFFFA3942, 0x8771F681, 0x6D9D6123, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
+            0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4D8A,
+            0xFFFA3942, 0x8771F681, 0x6D9D6122, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
             0xF4292244, 0x432AFF97, 0xAB9423A7, 0xFC93A039, 0x655B59C3, 0x8F0CCC92, 0xFFEFF47D, 0x85845DD1, 0x6FA87E4F, 0xFE2CE6E0, 0xA3014314, 0x4E0811A1, 0xF7537E82, 0xBD3AF235, 0x2AD7D2BB, 0xEB86D391,
         ]
         for offset in range(0, len(data), 64):
@@ -217,8 +200,8 @@ class QobuzMusicClient(BaseMusicClient):
     '''_parsewithgdstudioorgapi'''
     def _parsewithgdstudioorgapi(self, search_result: dict, request_overrides: dict = None):
         # init
-        host = "music.gdstudio.org"; version = "2026.07.21"; time_url = "https://music.gdstudio.org/time"; api_url = "https://music.gdstudio.org/api.php"
-        request_overrides, song_id, headers = request_overrides or {}, str(search_result['id']), {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36", "Origin": "https://music.gdstudio.org", "Referer": "https://music.gdstudio.org/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+        host = "music.gdstudio.org"; version = "2026.08.01"; time_url = "https://music.gdstudio.org/time"; api_url = "https://music.gdstudio.org/api.php"
+        request_overrides, song_id, headers = request_overrides or {}, str(search_result['id']), {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36", "Origin": "https://music.gdstudio.org", "Referer": "https://music.gdstudio.org/", "X-Requested-With": "XMLHttpRequest", "Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
         js_encode_uri_component_func = lambda value: (quote(str(value), safe="-_.!~*'()").replace("(", "%28").replace(")", "%29").replace("*", "%2A").replace("'", "%27").replace("!", "%21"))
         left_rotate_func = lambda x, amount: ((((x & 0xFFFFFFFF) << amount) | ((x & 0xFFFFFFFF) >> (32 - amount))) & 0xFFFFFFFF)
         normalize_version_func = lambda value: "".join(part.zfill(2) if len(part) == 1 else part for part in str(value).split("."))
@@ -231,8 +214,8 @@ class QobuzMusicClient(BaseMusicClient):
         shifts = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
         table = [
             0xD76AA478, 0xE8C7B756, 0x242070DB, 0xC1BDCEEE, 0xF57C0FAF, 0x4787C62A, 0xA8304613, 0xFD469501, 0x698098D8, 0x8B44F7AF, 0xFFFF5BB1, 0x895CD7BE, 0x6B901122, 0xFD987193, 0xA679438E, 0x49B40821,
-            0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4C8A,
-            0xFFFA3942, 0x8771F681, 0x6D9D6123, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
+            0xF61E2562, 0xC040B340, 0x265E5A51, 0xE9B6C7AA, 0xD62F105D, 0x02441453, 0xD8A1E681, 0xE7D3FBC8, 0x21E1CDE6, 0xC33707D6, 0xF4D50D87, 0x455A14ED, 0xA9E3E905, 0xFCEFA3F8, 0x676F02D9, 0x8D2A4D8A,
+            0xFFFA3942, 0x8771F681, 0x6D9D6122, 0xFDE5380C, 0xA4BEEA44, 0x4BDECFA9, 0xF6BB4B60, 0xBEBFBC70, 0x289B7EC6, 0xEAA127FA, 0xD4EF3085, 0x04881D05, 0xD9D4D039, 0xE6DB99E5, 0x1FA27CF8, 0xC4AC5665,
             0xF4292244, 0x432AFF97, 0xAB9423A7, 0xFC93A039, 0x655B59C3, 0x8F0CCC92, 0xFFEFF47D, 0x85845DD1, 0x6FA87E4F, 0xFE2CE6E0, 0xA3014314, 0x4E0811A1, 0xF7537E82, 0xBD3AF235, 0x2AD7D2BB, 0xEB86D391,
         ]
         for offset in range(0, len(data), 64):
@@ -256,8 +239,8 @@ class QobuzMusicClient(BaseMusicClient):
     '''_parsewiththirdpartapis'''
     def _parsewiththirdpartapis(self, search_result: dict, request_overrides: dict = None):
         if QobuzMusicClientUtils.get_token_func(self.default_headers, "X-User-Auth-Token", "x-user-auth-token"): return SongInfo(source=self.source, raw_data={'quality': QobuzMusicClientUtils.MUSIC_QUALITIES[-1]})
-        l1_parser_funcs = [self._parsewithgdstudioxyzapi, self._parsewithcyrusna29api, self._parsewithgdstudioorgapi, self._parsewithanandserverapi, self._parsewitharcodapi, self._parsewithspotbyeqzzapi, self._parsewithzarzqbzapi, ] # vip accounts
-        l2_parser_funcs = [] # vip accounts but unstable
+        l1_parser_funcs = [self._parsewithanandserverapi, self._parsewithzarzqbzapi, self._parsewithspotbyeqzzapi, ] # vip accounts
+        l2_parser_funcs = [self._parsewitharcodapi, self._parsewithgdstudioxyzapi, self._parsewithgdstudioorgapi, ] # vip accounts but unstable
         for parser_func in (l1_parser_funcs + l2_parser_funcs):
             song_info_flac = SongInfo(source=self.source, raw_data={'search': search_result, 'download': {}, 'lyric': {}, 'quality': QobuzMusicClientUtils.MUSIC_QUALITIES[-1]})
             with suppress(Exception): song_info_flac = parser_func(search_result, request_overrides)
